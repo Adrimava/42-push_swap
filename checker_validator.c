eb7ce2e54c9cb4ae30a810f1fbb1e8c7b3412508
Adrimava <adri_mv1@hotmail.com>
2021-04-16 15:42:02 +0200

    negative numbers validation

--- checker_validator.c
+++ checker_validator.c
@@ -6,7 +6,7 @@
 /*   By: amaza-va <amaza-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 17:59:11 by amaza-va          #+#    #+#             */
-/*   Updated: 2021/04/16 14:58:38 by amaza-va         ###   ########.fr       */
+/*   Updated: 2021/04/16 15:41:43 by amaza-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ int		array_length(char **arr)
 	return (length);
 }
 
-int		all_numbers_and_spaces(char *str)
+int		all_numbers_and_spaces_and_minus(char *str)
 {
 	while (*str)
 	{
-		if (!((*str >= '0' && *str <= '9') || *str == ' '))
+		if (!((*str >= '0' && *str <= '9') || *str == ' ' || *str == '-'))
 			return (0);
 		str++;
 	}
@@ -52,17 +52,36 @@ int		no_repeated_numbers(char **arr)
 	return (1);
 }
 
+int		negative_numbers_validation(char **arr)
+{
+	int		i;
+	char	*minus_position;
+	
+	i = 0;
+	while (i < array_length(arr))
+	{
+		if ((minus_position = ft_strrchr(arr[i], '-')))
+		{
+			if (!(*(minus_position + 1) >= '0' && *(minus_position + 1) <= '9'))
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
 int		param_validator(char *str)
 {
 	char	**params_split;
 
-	if (!all_numbers_and_spaces(str))
+	if (!all_numbers_and_spaces_and_minus(str))
 		return (0);
 	params_split = ft_split(str, ' ');
 	if (!no_repeated_numbers(params_split))
 		return (0);
+	if (!negative_numbers_validation(params_split))
+		return (0);
 	/**
-	 * TODO: negative numbers.
 	 * TODO: bigger than int.
 	 */
 	return (1);
