4b70737df98441a6a80a2e2193a5bb55b9eff635
Adrimava <adri_mv1@hotmail.com>
2021-04-17 22:28:23 +0200

    Graphic results

--- checker_result.c
+++ checker_result.c
@@ -6,7 +6,7 @@
 /*   By: amaza-va <amaza-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 21:31:35 by amaza-va          #+#    #+#             */
-/*   Updated: 2021/04/17 21:54:07 by amaza-va         ###   ########.fr       */
+/*   Updated: 2021/04/17 22:27:39 by amaza-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,59 @@ char	*ok_ko(t_stack **a, t_stack **b)
 	return ("OK");	
 }
 
+int		graphic_print(t_stack **stack)
+{
+	int		i;
+	int		max;
+	int		min;
+	t_stack	*tmp;
+
+	if (!(*stack))
+		return (0);
+	tmp = *stack;
+	max = (*stack)->number;
+	min = (*stack)->number;
+	i = 0;
+	while (i < list_size(tmp))
+	{
+		if (max < tmp->number)
+			max = tmp->number;
+		if (min > tmp->number)
+			min = tmp->number;
+		tmp = tmp->next;
+	}
+	return (min >= 0 && max <= 100);
+}
+
+void	stack_print(t_stack **stack)
+{
+	int		i;
+	int		j;
+	t_stack	*tmp;
+
+	tmp = *stack;
+	i = 0;
+	while (i < list_size(tmp))
+	{
+		j = 0;
+		while (j++ < (tmp)->number)
+			printf("▣");
+		printf("\n");
+		tmp = tmp->next;	
+	}
+}
+
 void	result(t_stack **a, t_stack **b)
 {
 	printf("===>a:\n");
-	print_list(*a);
-	printf("===>b:\n");
-	print_list(*b);
-	printf("%s\n", ok_ko(a, b));
+	if (graphic_print(a))
+		stack_print(a);
+	else
+		print_list(*a);
+	printf("\n===>b:\n");
+	if (graphic_print(b))
+		stack_print(b);
+	else
+		print_list(*b);
+	printf("\n%s\n", ok_ko(a, b));
 }
